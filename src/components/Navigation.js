f3ad663bbeacca1f6fb65c49aa4d031cd81e99b3
Wilber <wlunaj111@gmail.com>
2020-07-20 18:15:24 -0400

    Agregando el menuLinks de cada tipo de plato
    Se han agregado el menu en la parte inferior que sirve como link para cada tipos de platos en específico

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,15 +4,33 @@ import { Link } from 'react-router-dom'
 import './Navigation.css'
 
 export default class Navigation extends Component {
+
+
+    state = {
+        hidden: true,
+    }
+
+    showMenu = () => {
+        this.setState({hidden: this.state.hidden ? false : true})
+        console.log("Probando estadoooo",this.state.hidden)
+    }
+    StyleCompleted(){
+        if(!this.state.hidden){
+            return{
+                display: 'block',
+                transition: '1s'
+             }
+        }
+    }
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark alineacion">
+            <nav   className="navbar navbar-expand-lg navbar-dark bg-dark alineacion">
                 <div className="container">
                     <Link className="nav-link" to="/">NotesApp</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon" onClick={this.showMenu}></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div style={this.StyleCompleted()} className= "collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item active">
                                 <Link to="/listado" className="nav-link">Notes</Link>
