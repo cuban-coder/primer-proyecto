d8d5e2da4b705c7d418a48540434c20f5b1e0c39
Wilber <wlunaj111@gmail.com>
2020-07-22 12:25:24 -0400

    Probando las opciones de las tarjetas
    En las primeras dos cards hasy dos formas de hacer la peticion, modificar una de ellas y subirlo

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -2,20 +2,53 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import plat from "../img/imagen9.jpg";
-//import './responsive/responsive.js';
+
 
 import "./Listado2.css";
+import cards from './sample/cards.json'
+
 
 export default class Listado extends Component {
   state = {
-    style: document.querySelectorAll(".size"),
+    opciones: [1, 2, 3, 4],
+    active: 0,
+    cards: [
+      { 
+        id: 1,
+        active: false,
+        name: "active"
+      },
+      { 
+        id: 2,
+        active: false,
+        name: "active"
+      },
+      { 
+        id: 3,
+        active: false,
+        name: "active"
+      },
+      { 
+        id: 4,
+        active: false,
+        name: "active"
+      }
+     
+    ]
   };
 
-  componentDidMount() {
-    console.log("aqui va eso", this.state.style);
-  }
-
+  handleClick = async (id) => {
+    await this.setState({cards: this.state.cards.map(c => (c.id === id ? {...c, active: !c.active } : c))});
+    console.log("Probando id activo", this.state.cards);
+    }
+    handleClick2 = async (id) => {
+      await this.setState({active: id });
+      
+      }
+    
   render() {
+    const { opciones, active } = this.state;
+
     return (
       <div>
         <div className="containerPlato">
@@ -46,11 +79,71 @@ export default class Listado extends Component {
                   <div className="size-container">
                     <h3 className="title">cantidad</h3>
                     <div className="sizes">
-                      <span className="size">1</span>
-                      <span className="size">2</span>
-                      <span className="size active">3</span>
-                      <span className="size">4</span>
-                      <span className="size">5</span>
+                      {this.state.cards.map((c) => (
+                        <span
+                          className={
+                            "size " + (c.active === active ? "active" : "")
+                          }
+                          key={c.id}
+                          onClick={() => this.handleClick(c.id)}
+                        >
+                          {c.id}
+                        </span>
+                      ))}
+                      <span className="size">+</span>
+                    </div>
+                  </div>
+                  <div className="buy-price">
+                    <Link to="#" className="buy">
+                      <i className="fas fa-shopping-cart"></i>Add to card
+                    </Link>
+                    <div className="price">
+                      <i className="fas fa-dollar-sign"></i>
+                      <h1>189.99</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="colPlato">
+              <div className="cardPlato">
+                <div className="caja-imagen">
+                  <img src={plat} alt="" />
+                </div>
+                <div className="info">
+                  <div className="platoName">
+                    <div>
+                      <h1 className="big">Enchilada Italiana</h1>
+                      <span className="new">new</span>
+                    </div>
+                    <h3 className="small">
+                      algo sobre el plato que quiearas decir
+                    </h3>
+                  </div>
+                  <div className="description">
+                    <h3 className="title">Product info</h3>
+                    <p className="text">
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      In vel praesentium corporis iure? Blanditiis ipsa
+                      voluptates quo. Ex.
+                    </p>
+                  </div>
+                  <div className="size-container">
+                    <h3 className="title">cantidad</h3>
+                    <div className="sizes">
+                      {this.state.opciones.map((opcion) => (
+                        <span
+                          className={
+                            "size " + (opcion === active ? "active" : "")
+                          }
+                          key={opcion}
+                          onClick={() => this.handleClick2(opcion)}
+                        >
+                          {opcion}
+                        </span>
+                      ))}
+                      <span className="size">+</span>
                     </div>
                   </div>
                   <div className="buy-price">
@@ -158,7 +251,6 @@ export default class Listado extends Component {
               </div>
             </div>
 
-
             <div className="colPlato">
               <div className="cardPlato">
                 <div className="caja-imagen">
@@ -205,6 +297,7 @@ export default class Listado extends Component {
               </div>
             </div>
 
+           
 
             <div className="colPlato">
               <div className="cardPlato">
@@ -251,116 +344,6 @@ export default class Listado extends Component {
                 </div>
               </div>
             </div>
-
-
-            <div className="colPlato">
-              <div className="cardPlato">
-                <div className="caja-imagen">
-                  <img src={plat} alt="" />
-                </div>
-                <div className="info">
-                  <div className="platoName">
-                    <div>
-                      <h1 className="big">Enchilada Italiana</h1>
-                      <span className="new">new</span>
-                    </div>
-                    <h3 className="small">
-                      algo sobre el plato que quiearas decir
-                    </h3>
-                  </div>
-                  <div className="description">
-                    <h3 className="title">Product info</h3>
-                    <p className="text">
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                      In vel praesentium corporis iure? Blanditiis ipsa
-                      voluptates quo. Ex.
-                    </p>
-                  </div>
-                  <div className="size-container">
-                    <h3 className="title">cantidad</h3>
-                    <div className="sizes">
-                      <span className="size">1</span>
-                      <span className="size">2</span>
-                      <span className="size active">3</span>
-                      <span className="size">4</span>
-                      <span className="size">5</span>
-                    </div>
-                  </div>
-                  <div className="buy-price">
-                    <Link to="#" className="buy">
-                      <i className="fas fa-shopping-cart"></i>Add to card
-                    </Link>
-                    <div className="price">
-                      <i className="fas fa-dollar-sign"></i>
-                      <h1>189.99</h1>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
-            <div className="colPlato">
-              <div className="cardPlato">
-                <div className="caja-imagen">
-                  <img src={plat} alt="" />
-                </div>
-                <div className="info">
-                  <div className="platoName">
-                    <div>
-                      <h1 className="big">Enchilada Italiana</h1>
-                      <span className="new">new</span>
-                    </div>
-                    <h3 className="small">
-                      algo sobre el plato que quiearas decir
-                    </h3>
-                  </div>
-                  <div className="description">
-                    <h3 className="title">Product info</h3>
-                    <p className="text">
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                      In vel praesentium corporis iure? Blanditiis ipsa
-                      voluptates quo. Ex.
-                    </p>
-                  </div>
-                  <div className="size-container">
-                    <h3 className="title">cantidad</h3>
-                    <div className="sizes">
-                      <span className="size">1</span>
-                      <span className="size">2</span>
-                      <span className="size active">3</span>
-                      <span className="size">4</span>
-                      <span className="size">5</span>
-                    </div>
-                  </div>
-                  <div className="buy-price">
-                    <Link to="#" className="buy">
-                      <i className="fas fa-shopping-cart"></i>Add to card
-                    </Link>
-                    <div className="price">
-                      <i className="fas fa-dollar-sign"></i>
-                      <h1>189.99</h1>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
           </div>
         </div>
       </div>
