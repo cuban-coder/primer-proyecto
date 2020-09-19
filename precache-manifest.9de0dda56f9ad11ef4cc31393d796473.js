8ab5d0c42850caa2b86f8cac4543f53185d8ef26
Wilber <wlunaj111@gmail.com>
2020-09-19 19:27:03 -0400

    Updates

---
+++ precache-manifest.9de0dda56f9ad11ef4cc31393d796473.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4298cc5510310a0c7ce84e957a4bf6ae",
+    "url": "/primer-proyecto/index.html"
+  },
+  {
+    "revision": "21a6430c9a5d19d2524d",
+    "url": "/primer-proyecto/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c95b1c4234bcabe386b7",
+    "url": "/primer-proyecto/static/css/main.2984f496.chunk.css"
+  },
+  {
+    "revision": "21a6430c9a5d19d2524d",
+    "url": "/primer-proyecto/static/js/2.28548e42.chunk.js"
+  },
+  {
+    "revision": "c95b1c4234bcabe386b7",
+    "url": "/primer-proyecto/static/js/main.384264cf.chunk.js"
+  },
+  {
+    "revision": "2543495f4959b6c2eb2d",
+    "url": "/primer-proyecto/static/js/runtime~main.b7942bc8.js"
+  },
+  {
+    "revision": "16ecd168bcd49ebb700bf2bfdc0220c0",
+    "url": "/primer-proyecto/static/media/imagen7.16ecd168.jpg"
+  },
+  {
+    "revision": "973e38da8b1fa98e87d6b786b1b690ae",
+    "url": "/primer-proyecto/static/media/imagen9.973e38da.jpg"
+  }
+]);
